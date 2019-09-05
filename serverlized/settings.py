1ac81d3c179c5d00c6c0a985abb6b959538c7cb9
tonraj <rs188282@gmail.com>
2019-09-05 22:20:47 +0530

    Settings

--- serverlized/settings.py
+++ serverlized/settings.py
@@ -75,11 +75,17 @@ WSGI_APPLICATION = 'serverlized.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+'ENGINE': 'django.db.backends.mysql',
+'NAME': 'serverlized',
+'USER': 'root',
+'PASSWORD': "",
+'HOST': "localhost",
+'PORT': "",
+'OPTIONS': {
+'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
 }
-
+}
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -118,3 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'templates/'),
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'templates/media')
