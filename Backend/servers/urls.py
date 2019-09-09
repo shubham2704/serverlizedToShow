13b1ab7c0e3370d9d270e68f13431faae7484d67
tonraj <rs188282@gmail.com>
2019-09-09 20:29:35 +0530

    DEPLOY SERVER

--- Backend/servers/urls.py
+++ Backend/servers/urls.py
@@ -1,7 +1,20 @@
-from django.urls import path
+from django.urls import path, include
+from ..BackendController.server_config import PACKAGES
+from django.utils.module_loading import import_string
 from . import views
 
 urlpatterns = [
     path('wpanel/', views.panel, name='User Sign Up, Code => /Backend/servers/'),
-    path('wpanel/deploy', views.deploy, name='Deploy Server, Code => /Backend/servers/')
+    path('wpanel/deploy', views.deploy, name='Deploy Server, Code => /Backend/servers/'),
 ]
+
+control_url = []
+
+for key, pkg in PACKAGES.items():
+    control = pkg['CONTROL_PANEL']
+    for key, cntr in control.items():
+        for key_a, cop in cntr.items():
+            
+            control_url.append(path(cop['URL'][0], import_string(cop['URL'][1])))
+            
+urlpatterns += control_url
--- serverlized/urls.py
+++ serverlized/urls.py
@@ -19,11 +19,13 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('', include('Backend.signup.urls')),
     path('', include('Backend.userLogin.urls')),
     path('', include('Backend.servers.urls')),
 ]
+control_url = []
 
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
