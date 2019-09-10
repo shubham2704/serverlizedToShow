598dfe4de19c0ff5864b3af863da19b3a468c8bc
tonraj <rs188282@gmail.com>
2019-09-11 00:31:02 +0530

    TEST

--- Backend/BackendController/tasks/install_stack.py
+++ Backend/BackendController/tasks/install_stack.py
@@ -46,13 +46,14 @@ def installStack(insert_id = 0):
                         sftp.put(file_upload, "/etc/serverlized/" + ntpath.basename(file_upload))
                         print(" cd  /etc/serverlized/; ./" + ntpath.basename(file_upload))
                         print(" cd  /etc/serverlized/; chmod +x " + ntpath.basename(file_upload))
+                        sendNotification(get_server.user_id.id, 'toast', 'success', 'Started Installing', '<b>'+ PACKAGES[pkg_id]['NAME'] +'</b> is started installing on ' + get_server.server_name + '  (' + get_server.server_ip + ').')
                         client.exec_command("cd  /etc/serverlized/; chmod +x " + ntpath.basename(file_upload))
                         stdidn,stddout,stdderr=client.exec_command(" cd  /etc/serverlized/; ./" + ntpath.basename(file_upload))
                         client.exec_command( SERVER_OS_DISTRIBUTION[os_id][2] + " rm /etc/serverlized/" + ntpath.basename(file_upload))
                         print ("stderr: ", stdderr.readlines())
                         print ("pwd: ", stddout.readlines())
                         print ("INSTALLED : " +  ntpath.basename(file_upload))
-                        sendNotification(get_server.user_id.id, 'toast', 'success', 'Package Installed', '<b>'+ PACKAGES[pkg_id]['NAME'] +'</b> is succesfully installed in ' + get_server.server_name + '  (' + get_server.server_ip + ').')
+                        sendNotification(get_server.user_id.id, 'toast', 'success', 'Package Installed', '<b>'+ PACKAGES[pkg_id]['NAME'] +'</b> is succesfully installed on ' + get_server.server_name + '  (' + get_server.server_ip + ').')
         
                         
 
